e784882b394e0a208aaaeb33b12889c161aa83f4
tsubasa <mc0627tsubasa283@gmail.com>
2023-11-21 04:30:37 +0900

    クマラジに日付を指定できるオプションを追加

--- commands/radio.js
+++ commands/radio.js
@@ -4,11 +4,22 @@ const {createKumaRadio} = require("../source/kuma-radio")
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("radio")
-        .setDescription("クマラジを作成します。"),
+        .setDescription("クマラジを作成します。")
+        .addStringOption(option=>
+            option.setName("when")
+                .setDescription("オプション")
+                .addChoices(
+                    { name: 'Today', value: 'Today' },
+                    { name: 'Tomorrow', value: 'Tomorrow' })),
     execute: async (interaction,client) => {
+        let option_ = interaction.options.getString("when") ?? "Today";
         await interaction.reply("クマラジを作成します。");
         await new Promise(s => setTimeout(s, 1000));
         await interaction.deleteReply();
-        createKumaRadio(client,0, interaction.channel.id);
+        if(option_ == "Today"){
+            createKumaRadio(client,0, interaction.channel.id);
+        }else if (option_ == "Tomorrow"){
+            createKumaRadio(client,1, interaction.channel.id);
+        }
     }
 }
