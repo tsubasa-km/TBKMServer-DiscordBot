2d09d4df912b45eaa90e291f2d480b5eb23834c8
tsubasa <mc0627tsubasa283@gmail.com>
2024-01-08 22:14:21 +0900

    バグ修正

--- source/kuma-radio.js
+++ source/kuma-radio.js
@@ -2,17 +2,16 @@ const { createThread, JsonDBManager } = require("./util");
 
 async function kumaRadioSubscribe(client, addDays = 0, channel_id = null) {
     let date = new Date();
+    date.setDate(date.getDate() + addDays);
     if (channel_id) {
-        date.setDate(date.getDate() + addDays);
         createThread(client, channel_id, `クマラジ ${date.getMonth() + 1}.${date.getDate()}`);
         return;
     }
     let subscribe_channel_ids = await new JsonDBManager("subscribe-channel").read();
-    console.log(subscribe_channel_ids["kuma-radio"]);
-    subscribe_channel_ids["kuma-radio"].forEach(thread_channelId => {
-        date.setDate(date.getDate() + addDays);
-        createThread(client, thread_channelId, `クマラジ ${date.getMonth() + 1}.${date.getDate()}`);
+    subscribe_channel_ids["kuma-radio"].forEach(channel_id_ => {
+        createThread(client, channel_id_, `クマラジ ${date.getMonth() + 1}.${date.getDate()}`);
     })
+    delete date
 }
 
 module.exports = { kumaRadioSubscribe };
